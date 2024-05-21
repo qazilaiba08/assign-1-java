632ce68f3c6610c0490aae5fa76a8d9bc79af506
Laiba Qazi <164245574+qazilaiba08@users.noreply.github.com>
2024-05-21 13:13:35 +0500

    dsa

---
+++ jscript.js
@@ -0,0 +1,83 @@
+//Q # 1 & 2
+
+// alert("JS alert box");
+
+// alert("Error please enter a valid password");
+
+// //Q # 3
+
+// alert("Welcome to JS land... \n Happy Coding!");
+
+// //Q#4
+
+// alert("Welcome to JS land...");
+// alert("prevrent this  page creating addional dialogais");
+
+//Q #5 
+
+// alert("Hello... i can run JS through my web browser console");
+
+// console.log(alert);
+
+// //Q # 6 and 7
+
+// // chap 2 
+
+
+// //Q #1
+// var username ;//declear
+
+//Q#2
+
+// var myname = "Laiba Qazi";
+// document.write(myname + "<br>");
+
+//Q#3
+//a
+// var JS_Vairable ;//declear
+
+// //b 
+// var message = "Hello World";
+// document.write(message+ "<br>");
+
+// //c 
+// alert(message);
+
+//Q#4
+
+var student = "Jhon Doe";
+alert(student);
+
+var age = 15 +"years old";
+alert(age);
+
+var Qulifaction = "Mobile App Development";
+alert(Qulifaction);
+
+//Q#5
+
+var pizza = "PIZZA"
+alert(pizza +"\n" + "PIZZ"+"\n"+"PIZ"+"\n"+"PI"+"\n"+"P");
+
+//Q#6
+
+var email = "example@example.com";//declear
+
+alert("My Email address is"+" "+email);
+
+// Q#7
+var book = "A Smarter way to learn JavaScript";
+alert("I Am trying to learn from the Book "+" "+book);
+
+//Q#8 
+
+document.write("Yah!I can write  HTML content through JavaScript ");
+
+//Q#9
+
+var strin = '▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬';
+document.write(strin);
+alert(strin);
+
+
+
